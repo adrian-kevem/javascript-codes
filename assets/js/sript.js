a3902bffa4526cbd9ce4c615d212aaa92a1c81a8
Adrian Kevem <adrianveven@gmail.com>
2022-10-20 08:30:48 -0300

    Init

---
+++ assets/js/sript.js
@@ -0,0 +1,15 @@
+alert("Hello World!");
+console.log("Hello World!");
+let nome = "Adrian";
+let idade = 21;
+let altura = 1.75;
+let logado = true;
+let tecnologias = ["Python", "JavaScript", "TypeScript"]
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Altura: " + altura);
+console.log("Logado: " + logado);
+console.log(tecnologias)
+let dados = {nome: "Adrian", idade: 21}
+console.log(dados["nome"]);
+console.log(typeof dados);
